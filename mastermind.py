dcac8bf47b196416d4a322b15de4ed64712a0290
TheNobleOne <thenoblejmn@gmail.com>
2015-08-11 19:53:26 -0700

    Fixed an iteration issue for peg colors
    Looks like I accidentally mixed the iteration values for the loops and
    thus resulting in a situation where 4 guesses of a single color with 1
    matching color in the master list would always return 1 black and 3
    white peg color fills.  Renamed the iteration variable to make their
    use clearer.

--- mastermind.py
+++ mastermind.py
@@ -6,7 +6,9 @@
 from graphics import *
 import random
 
+#
 def revealSolution (master, win):
+    # Sets the base x and y coordinate values, uses the turn count to increase the y value so that it's on a newline
     mColor = [None] * 4
     x, y = 9, 28
 
@@ -16,7 +18,9 @@ def revealSolution (master, win):
         mColor[i].draw (win)
         x += 4
 
+# Function a pop up window indicating whether the user was able to correctly guess the master code or not
 def finishedCondition (score, win):
+
     popUp = Rectangle (Point (5, 10), Point (25, 20,))
     popUp.setFill ('lightskyblue')
     popUp.draw (win)
@@ -33,10 +37,9 @@ def finishedCondition (score, win):
         message.setText ("Sorry, you lose!")
         message.draw (win)
 
+# Function sets peg colors to the indeed information
 def drawPegs (black, white, peg, win):
-    print ("black", black)
-    print ("white", white)
-
+    #runs 4 times, setting the fill colors as needed
     for i in range (len (peg)):
         if black > 0:
             peg[i].setFill ('black')
@@ -50,23 +53,27 @@ def ansCompare (master, guess):
     bCount = 0
     wCount = 0
 
-    for i in range (len (guess)):
-        if guess[i] == master[i]:
-            guess[i] = 'black'
+    #bp == black peg iteration thru list of master color list
+    for bp in range (len (master)):
+        if guess[bp] == master[bp]:
+            guess[bp] = 'black'
         else:
-            for j in range (len (guess)):
-                if master[j] == guess[i] and guess[i] != 'black':
-                    guess[i] = 'white'
-                print ("g{0} = {1}".format (i, guess))
-                print ("m{0} = {1}".format (i, master))
+            #wp == white peg iteration comparing the value in master to the rest of the values in guess code
+            for wp in range (len (master)):
+                if master[bp] == guess[wp] and guess[wp] != 'black':
+                    guess[wp] = 'white'
 
+    # each count sends int value of each type of peg to function call
     return guess.count ('black'), guess.count ('white')
 
+# Replaces the 4 entry boxes with 4 circles filled with the colors guessed by the user
 def drawGuess (win, boxes, turn, color):
+    # Sets the base x and y coordinate values, uses the turn count to increase the y value so that it's on a newline
     x, y = 9, 3
     if turn > 0:
         y = y + (2 * turn)
 
+    # Draws 4 cirlces
     for i in range (len (boxes)):
         boxes[i].undraw ()
         guessDot = Circle (Point (x,y), .75)
@@ -74,7 +81,7 @@ def drawGuess (win, boxes, turn, color):
         guessDot.draw (win)
         x += 4
 
-#function to convert single letter inputs into the needed fill color values
+#function to convert single letter inputs into the needed fill color strings
 def colorConvert (text, win, turn):
     for i in range (len (text)):
         if text[i].lower () == 'o':
@@ -89,8 +96,6 @@ def colorConvert (text, win, turn):
             text[i] = 'yellow'
         elif text[i].lower () == 'b':
             text[i] = 'blue'
-        else:
-            entryBoxes (win, turn)
     return text
 
 # gets the user inputed color letters
@@ -105,34 +110,43 @@ def getGuess (win, info):
 # Create 4 pegs
 def createPegs (win, turn):
     pegDot = [None] * 4
+    # Sets the base x and y coordinate values, uses the turn count to increase the y value so that it's on a newline
     x, y = 6, 2.5
     if turn > 0:
         y = y + (3 * turn)
 
+    #Creates 4 small cirlces to represent the information pegs
     for i in range (4):
         pegDot[i] = Circle (Point (x, y), .5)
         pegDot[i].setFill ('dimgray')
         pegDot[i].draw (win)
+
+        # Adjusts the x & y values to create a 2 x 2 square of circles
         if i == 0 or i == 2:
             x += 1.25
         else:
             x -= 1.25
             y += 1.25
+
+    # Sends a list with the circles for futher manipulation
     return pegDot
 
-# creates the 4 needed entry boxes
+# Draws and creates entry boxes and sets them to a list that is returned to main
 def entryBoxes (win, turn):
+    # Sets the base x and y coordinate values, uses the turn count to increase the y value so that it's on a newline
     entryBox = [None] * 4
     x, y = 9, 3
     if turn > 0:
         y = y + (2 * turn)
 
+    # Creates 4 entry boxes for the user's guesses
     for i in range (4):
         entryBox[i] = Entry (Point (x, y), 4)
         entryBox[i].setText ('')
         entryBox[i].draw (win)
         x += 4
 
+    # Sends a list with the circles for futher manipulation
     return entryBox
 
 # randomly creates the 4 color code to be guessed by the user
@@ -140,11 +154,12 @@ def masterCode ():
     masterColors = []
     colorList = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 
+    # Creates a list of 4 random colors to be used as the master code for the user to guess
     for i in range (4):
         masterColors.append (random.choice (colorList))
     return masterColors
 
-# Creates submit button
+# Creates a submit button icon
 def createButton (win):
     subButton = Rectangle (Point (14, 1.5), Point (16, .5))
     subButton.setFill ('lightgray')
@@ -164,11 +179,13 @@ def createWindow ():
 
 #runs all the functions
 def main():
+    # Creates and setups the inital parameters needed for the game
     turnCount = 0
     gameWin = createWindow ()
     colorMasterList = masterCode ()
-    print (colorMasterList)
+    print (colorMasterList) #<---- this line for testing only!!!
 
+    # For loop set to the number of guesses a user is allowed
     for turnCount in range (10):
         pegCircles = createPegs (gameWin, turnCount)
         boxes = entryBoxes (gameWin, turnCount)
@@ -177,12 +194,14 @@ def main():
         drawGuess (gameWin, boxes, turnCount, colorGuessList)
         bPeg, wPeg = ansCompare (list (colorMasterList), list (colorGuessList))
 
+    # The decision tree determines whether the user has won, loss, or has another round of guessing
         if bPeg == 4:
             revealSolution (list (colorMasterList), gameWin)
             result = finishedCondition ('victory', gameWin)
             break
         elif turnCount == 9:
             result = finishedCondition ('lost', gameWin)
+            break
         else:
             drawPegs (bPeg, wPeg, pegCircles, gameWin)
 
